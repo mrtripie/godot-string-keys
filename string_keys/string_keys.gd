daf10982da1f1440f40b6e3525057f818ccc3883
MrTriPie <mrtripiedev@gmail.com>
2020-05-15 22:05:21 -0400

    Renamed files from ThisFormat to this_format for consistency with rest of engine

--- string_keys/string_keys.gd
+++ string_keys/string_keys.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 var dock
 
 func _enter_tree():
-	dock = preload("res://addons/StringKeys/StringKeysDock.tscn").instance()
+	dock = preload("res://addons/string_keys/string_keys_dock.tscn").instance()
 	add_control_to_dock(DOCK_SLOT_RIGHT_UR, dock)
 
 func _exit_tree():
